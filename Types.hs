5a5f507f92f0021bab138c61453bb8550fbffc45
TheSola10 <me@thesola.io>
2021-11-10 10:41:16 +0100

    Fixed Ord implementation

--- Types.hs
+++ Types.hs
@@ -1,20 +1,19 @@
 module Types(   Type(Bare, Application, Wide),
                 TypeAtom(TSet, TList, TFile, TText, TBool, TInt, TFloat, TNull, TNamed)) where
 
-data Type       = Bare          TypeAtom            -- T
-                | Application   Type Type           -- T -> (...)
+data Type       = Application   Type Type           -- T -> (...)
+                | Bare          TypeAtom            -- T
                 | Wide          [ TypeAtom ]        -- (T or T)
 
-data TypeAtom   = TSet  [ (String, Type) ]          -- { (T) a, (T) b }
+data TypeAtom   = TNamed String Type                -- T :: ...;
                 | TList Type                        -- ([T])
+                | TSet  [ (String, Type) ]          -- { (T) a, (T) b }
+                | TBool                             -- (Bool)
+                | TNull                             -- (Null)
+                | TFloat                            -- (Float)
+                | TInt                              -- (Int)
                 | TFile                             -- (File)
                 | TText                             -- (Text)
-                | TBool                             -- (Bool)
-                | TInt                              -- (Int)
-                | TFloat                            -- (Float)
-                | TNull                             -- (Null)
-                | TNamed String Type                -- T :: ...;
-
 
 instance Eq Type where
     (Bare a)            == (Bare b)             = a == b
@@ -23,23 +22,21 @@ instance Eq Type where
     _                   == _                    = False
 
 instance Ord Type where
-    Application _ _                 < Bare _                    = True
-    Bare _                          < Wide _                    = True
-    Bare t1                         < Bare t2                   = t1 < t2
-    Application _ (Application _ _) < Application _ (Bare _)    = True
-    Application a1 t1               < Application a2 t2         = (a1 < a2) || (t1 < t2)
-    Wide l1                         < Wide l2                   = length l1 < length l2
-                                                                || and (zipWith (<) l1 l2)
+    Wide _              <=  Bare _              = False
+    Bare _              <=  Application _ _     = False
 
-    a < b = not (b <= a)
-    a <= b = (a == b) || (a < b)
+    Bare t1             <=  Bare t2             = t2 < t1
+    Application a1 t1   <=  Application a2 t2   = (a2 < a1) || (t2 < t1)
+    Wide l1             <=  Wide l2             = length l2 < length l1
+                                                  || and (zipWith (<) l2 l1)
 
+    _ <= _ = True
 
 instance Eq TypeAtom where
     TList t1        == TList t2     = t1 == t2
     TNamed n1 t1    == TNamed n2 t2 = (n1 == n2) && (t1 == t2)
     TSet l1         == TSet l2      = and $ zipWith tcompare l1 l2
-                                    where tcompare (s1, t1) (s2, t2) = (s1 == s2) && (t1 == t2)
+                                      where tcompare (s1, t1) (s2, t2) = (s1 == s2) && (t1 == t2)
     TFile   ==  TFile   = True
     TText   ==  TText   = True
     TBool   ==  TBool   = True
@@ -49,19 +46,17 @@ instance Eq TypeAtom where
     _       ==  _       = False
 
 instance Ord TypeAtom where
-    TNamed _ _  <   TList _     = True
-    TList _     <   TSet _      = True
-    TSet _      <   TBool       = True
-    TBool       <   TNull       = True
-    TNull       <   TFloat      = True
-    TFloat      <   TInt        = True
-    TInt        <   TFile       = True
-    TFile       <   TText       = True
+    TText       <=  TFile       = False
+    TFile       <=  TInt        = False
+    TInt        <=  TFloat      = False
+    TFloat      <=  TNull       = False
+    TNull       <=  TBool       = False
+    TBool       <=  TSet _      = False
+    TSet _      <=  TList _     = False
+    TList _     <=  TNamed _ _  = False
 
-    TList t1    <   TList t2    = t1 < t2
-    TSet l1     <   TSet l2     = length l1 > length l2 || and (zipWith tsort l1 l2)
-                                where tsort (_, t1) (_, t2) = t1 < t2
+    TList t1    <=  TList t2    = t2 < t1
+    TSet l1     <=  TSet l2     = length l2 > length l1 || and (zipWith tsort l2 l1)
+                                  where tsort (_, t1) (_, t2) = t1 < t2
 
-    a < b = not (b <= a)
-    a <= b = (a == b) || (a < b)
-
+    _           <=  _           = True
